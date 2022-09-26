a36f0aaba5fd40e38a215e477623152aa8cd005f
Rodrigo <rodrigosndsilva@gmail.com>
2022-09-26 12:17:12 +0100

    Git Repo Api

---
+++ app/src/main/java/com/rodrigosnds/gitrepo/data/remote/GitRepoApi.kt
@@ -0,0 +1,19 @@
+package com.rodrigosnds.gitrepo.data.remote
+
+import com.rodrigosnds.gitrepo.domain.model.Repository
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface GitRepoApi {
+    @GET("search/repositories")
+    suspend fun getListOfRepos(
+        @Query("q") query: String,
+        @Query("sort") sort: String,
+        @Query("order") order: String
+    ): List<Repository>
+
+    @GET("users/{users}/repos")
+    suspend fun getListOfUsers(@Path("users") users: String): List<Repository>
+}
