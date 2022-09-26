5e9d458678736ae3c22b38d3ff3fde5ec9ce9cd5
Rodrigo <rodrigosndsilva@gmail.com>
2022-09-26 12:17:20 +0100

    repository

---
+++ app/src/main/java/com/rodrigosnds/gitrepo/data/repository/GitRepoRepository.kt
@@ -0,0 +1,17 @@
+package com.rodrigosnds.gitrepo.data.repository
+
+import com.rodrigosnds.gitrepo.data.remote.GitRepoApi
+import com.rodrigosnds.gitrepo.domain.model.Repository
+import javax.inject.Inject
+
+class GitRepoRepository @Inject constructor(
+    private val api: GitRepoApi
+) {
+    suspend fun getListOfRepos(query: String, sort: String, order: String): List<Repository> {
+        return api.getListOfRepos(query, sort, order)
+    }
+
+    suspend fun getListOfUsers(user: String): List<Repository> {
+        return api.getListOfUsers(user)
+    }
+}
