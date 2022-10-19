2235bf284091fd0c766b51e11c142c9ca6bbcdee
Rodrigo <rodrigosndsilva@gmail.com>
2022-10-19 15:51:33 +0100

    formatting

--- app/src/main/java/com/rodrigosnds/gitrepo/common/GitRepoApplication.kt
+++ app/src/main/java/com/rodrigosnds/gitrepo/common/GitRepoApplication.kt
@@ -3,5 +3,4 @@ package com.rodrigosnds.gitrepo.common
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
-class GitRepoApplication : Application()
+@HiltAndroidApp class GitRepoApplication : Application()
