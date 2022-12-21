f5b8eaaa84cb79be2fda7e14c42b7ba0fd24fd2b
Rodrigo Silva <rodrigosndsilva@gmail.com>
2022-12-21 11:23:19 +0000

    Firebase (#5)
    Co-authored-by: RafaelBelo14 <rafael14belo@gmail.com>

--- app/src/main/java/com/rodrigosnds/gitrepo/common/GitRepoApplication.kt
+++ app/src/main/java/com/rodrigosnds/gitrepo/common/GitRepoApplication.kt
@@ -1,6 +1,22 @@
 package com.rodrigosnds.gitrepo.common
 
 import android.app.Application
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.rodrigosnds.gitrepo.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp class GitRepoApplication : Application()
+@HiltAndroidApp
+class GitRepoApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        //configCrashAndAnalyticsReport()
+    }
+
+    private fun configCrashAndAnalyticsReport() {
+        if (BuildConfig.DEBUG) {
+            FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(false)
+            FirebaseAnalytics.getInstance(this).setAnalyticsCollectionEnabled(false)
+        }
+    }
+}
--- app/src/main/java/com/rodrigosnds/gitrepo/ui/homescreen/Homescreen.kt
+++ app/src/main/java/com/rodrigosnds/gitrepo/ui/homescreen/Homescreen.kt
@@ -3,10 +3,7 @@ package com.rodrigosnds.gitrepo.ui.homescreen
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -35,81 +32,78 @@ fun Homescreen(
 ) {
     val state by viewModel.state.collectAsState()
     Column {
+        Button(onClick = { throw RuntimeException("Test Crash") }) {
+            Text(text = "Test")
+        }
         UserRepoNavbar(viewModel)
         Box(modifier = Modifier.fillMaxSize()) {
             LazyColumn(modifier = Modifier.fillMaxSize()) {
                 items(state.repoList) { repo ->
-                    DefaultCard(
-                        onItemClick = {
-                            navigator.navigate(
-                                RepoDetailsDestination(repo.owner.ownerName, repo.name),
-                                onlyIfResumed = true
-                            )
-                        },
-                        content = {
-                            Box(
-                                modifier = Modifier
-                                    .padding(15.dp)
-                                    .padding(top = 10.dp, bottom = 10.dp)
+                    DefaultCard(onItemClick = {
+                        navigator.navigate(
+                            RepoDetailsDestination(repo.owner.ownerName, repo.name),
+                            onlyIfResumed = true
+                        )
+                    }, content = {
+                        Box(
+                            modifier = Modifier
+                                .padding(15.dp)
+                                .padding(top = 10.dp, bottom = 10.dp)
+                        ) {
+                            Column(
+                                modifier = Modifier.padding(15.dp)
                             ) {
-                                Column(
-                                    modifier = Modifier.padding(15.dp)
+                                Row(
+                                    Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.Start
                                 ) {
-                                    Row(
-                                        Modifier.fillMaxWidth(),
-                                        horizontalArrangement = Arrangement.Start
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.github_user),
+                                        contentDescription = stringResource(id = R.string.content_description_user_icon)
                                     )
-                                    {
-                                        Icon(
-                                            painter = painterResource(id = R.drawable.github_user),
-                                            contentDescription = stringResource(id = R.string.content_description_user_icon)
-                                        )
-                                        Text(
-                                            text = repo.name,
-                                            style = MaterialTheme.typography.body1,
-                                            overflow = TextOverflow.Ellipsis,
-                                            modifier = Modifier.padding(start = 5.dp)
-                                        )
-                                    }
-                                    Spacer(modifier = Modifier.padding(10.dp))
-                                    Row(
-                                        Modifier.fillMaxWidth(),
-                                        horizontalArrangement = Arrangement.Start
+                                    Text(
+                                        text = repo.name,
+                                        style = MaterialTheme.typography.body1,
+                                        overflow = TextOverflow.Ellipsis,
+                                        modifier = Modifier.padding(start = 5.dp)
                                     )
-                                    {
-                                        Icon(
-                                            painter = painterResource(id = R.drawable.github_description),
-                                            contentDescription = stringResource(id = R.string.content_description_icon)
-                                        )
-                                        Text(
-                                            text = repo.description
-                                                ?: stringResource(id = R.string.no_description),
-                                            style = MaterialTheme.typography.body1,
-                                            overflow = TextOverflow.Ellipsis,
-                                            modifier = Modifier.padding(start = 5.dp)
-                                        )
-                                    }
-                                    Spacer(modifier = Modifier.padding(10.dp))
-                                    Row(
-                                        Modifier.fillMaxWidth(),
-                                        horizontalArrangement = Arrangement.Start
+                                }
+                                Spacer(modifier = Modifier.padding(10.dp))
+                                Row(
+                                    Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.Start
+                                ) {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.github_description),
+                                        contentDescription = stringResource(id = R.string.content_description_icon)
                                     )
-                                    {
-                                        Icon(
-                                            painter = painterResource(id = R.drawable.github_stars),
-                                            contentDescription = stringResource(id = R.string.content_description_stars_icon)
-                                        )
-                                        Text(
-                                            text = "${repo.stargazersCount}",
-                                            style = MaterialTheme.typography.body1,
-                                            overflow = TextOverflow.Ellipsis,
-                                            modifier = Modifier.padding(start = 5.dp)
-                                        )
-                                    }
+                                    Text(
+                                        text = repo.description
+                                            ?: stringResource(id = R.string.no_description),
+                                        style = MaterialTheme.typography.body1,
+                                        overflow = TextOverflow.Ellipsis,
+                                        modifier = Modifier.padding(start = 5.dp)
+                                    )
+                                }
+                                Spacer(modifier = Modifier.padding(10.dp))
+                                Row(
+                                    Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.Start
+                                ) {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.github_stars),
+                                        contentDescription = stringResource(id = R.string.content_description_stars_icon)
+                                    )
+                                    Text(
+                                        text = "${repo.stargazersCount}",
+                                        style = MaterialTheme.typography.body1,
+                                        overflow = TextOverflow.Ellipsis,
+                                        modifier = Modifier.padding(start = 5.dp)
+                                    )
                                 }
                             }
                         }
-                    )
+                    })
                 }
             }
             state.error?.let {
